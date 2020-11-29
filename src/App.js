b0d43dffafa8b9cdd148e8a063e0025b5b056b36
Kyle Fallenbaum <kylefallenbaum@Kyles-MacBook-Pro-2.local>
2020-11-29 15:02:57 -0500

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import AboutUs from "./pages/AboutUs";
+//GlobalStyle
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
